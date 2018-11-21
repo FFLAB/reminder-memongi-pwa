c72e28809f9a85ae7e959587161c18ef2070e065
Phil Mass <philrmass@gmail.com>
2018-11-20 18:31:52 -0800

    add cache name

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+const cacheName = "reminder-cache";
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open("reminder-cache").then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
       console.log("add to cache");
       return cache.add("/reminder-pwa/index.html");
     })
@@ -11,7 +13,7 @@ self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       let found = (response ? "found" : "MISSING");
-      console.log("cache-get " + found + ":", event.request.url);
+      console.log("cache-get," + found + ":", event.request.url);
       return response || fetch(event.request);
     })
   );
