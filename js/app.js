4c72bcfb1feccd519867790e28a5705928d3379c
Phil Mass <philrmass@gmail.com>
2018-11-20 17:02:28 -0800

    move link to html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("dom loaded");
-
-  if("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("../sw.js")
-      .then(function() { console.log("Registered service worker"); });
-  }
 });
